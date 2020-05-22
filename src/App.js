7740603b6c075c61cc29f45a56a172a781826546
olateekay <olatokunboalege@gmail.com>
2020-05-23 00:02:43 +0100

    fetching

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      monsters:[
-        {
-          name:"Frankestein",
-          id:"1"
-        },
-        {
-          name:"Dracula",
-          id:"2"
-        },
-        {
-          name:"Zombie",
-          id:"3"
-        }
-      ]
+      monsters:[]
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then(users => this.setState({monsters:users}))
   }
   render (){
     return (
