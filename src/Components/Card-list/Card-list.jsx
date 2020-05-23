498b85855f0c0da13c5816a09b853527234ea3a3
olateekay <olatokunboalege@gmail.com>
2020-05-23 01:36:15 +0100

    Card component

--- src/Components/Card-list/Card-list.jsx
+++ src/Components/Card-list/Card-list.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Card-list.css'
+import { Card } from '../Card/Card';
 
 export const CardList = props => {
-  console.log(props);
+  
   return(
-  <div className='card-list'>{props.children}</div>
+  <div className='card-list'> {props.monsters.map(monster => (
+    <Card key={monster.id} monster={monster} />
+  ))}
+  </div>
   )
-    };
+};
