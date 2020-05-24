6ebbe52d57c73b732d647cb9dac34c348d8c0263
olateekay <olatokunboalege@gmail.com>
2020-05-24 16:19:32 +0100

    Added styling

--- src/App.js
+++ src/App.js
@@ -18,16 +18,24 @@ class App extends React.Component {
     .then( response => response.json())
     .then(users => this.setState({monsters:users}))
   }
+
+  handleChange = e => {
+    this.setState({searchField:e.target.value})
+  }
+
   render (){
+
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
       );
+
     return (
       <div className="App">
+      <h1>Monsters</h1>
       <SearchBox 
         placeholder="Search Monsters"
-        handleChange={e=>this.setState({searchField: e.target.value})}
+        handleChange={this.handleChange}
       />
       <CardList monsters={filteredMonsters}/>
      
